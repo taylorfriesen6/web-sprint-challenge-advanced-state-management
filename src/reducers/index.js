00e9f9ae4aa235ed4fb79d0a8dbb9a9b5b2a4f26
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-19 11:52:14 -0700

    removed test data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,16 +6,16 @@ import {
   SET_ERROR,
 } from '../actions'
 
-const testSmurf = {
-  id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-  name:'Poppa Smurf',
-  position:'Village Leader',
-  nickname: 'Pops',
-  description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-}
+// const testSmurf = {
+//   id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+//   name:'Poppa Smurf',
+//   position:'Village Leader',
+//   nickname: 'Pops',
+//   description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
+// }
 
 export const initialState = {
-  smurfs: [testSmurf, testSmurf],
+  smurfs: [],
   isLoading: false,
   err: ''
 }
