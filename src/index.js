7bb9c611024722940ea2a1b5236d7e4b989f8945
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-19 10:15:37 -0700

    redux store created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from './reducers';
+
 import "./index.css";
 import App from "./App";
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}><App /></Provider>, 
     rootElement
 );
 
